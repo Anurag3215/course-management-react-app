d568f7d14b22e371d254adae127aa0488f82f6d6
Anurag3215 <anuragaacc@gmail.com>
2026-07-11 15:10:59 +0530

    View updated

--- src/components/InsertCourse.jsx
+++ src/components/InsertCourse.jsx
@@ -1,53 +1,53 @@
 import React from 'react'
 
 const InsertCourse = () => {
-  return (
-    <div>
+    return (
+        <div>
 
-<div className="container">
-    <div className="row">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-<div className="row">
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-<label htmlFor="" className="form-label">Course Name</label>
-<input type="text" className="form-form" />
+                                <label htmlFor="" className="form-label">Course Name</label>
+                                <input type="text" className="form-form" />
 
-    </div>
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        
-<label htmlFor="" className="form-label">Duration</label>
-<input type="text" className="form-form" />
-</div>
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        
-<label htmlFor="" className="form-label">Fee</label>
-<input type="text" className="form-form" />
-</div>
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        
-<label htmlFor="" className="form-label">Mode</label>
-<select name="" id="" className="form-conrol">Online</select>
-<select name="" id="" className="form-conrol">Offline</select>
-<select name="" id="" className="form-conrol">Hybrid</select>
-</div>
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        
-<label htmlFor="" className="form-label">Trainer</label>
-<input type="text" className="form-form" />
-</div>
-    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        <button className="btn btn-primary">Submit</button>
-    </div>
-</div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <label htmlFor="" className="form-label">Duration</label>
+                                <input type="text" className="form-form" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <label htmlFor="" className="form-label">Fee</label>
+                                <input type="text" className="form-form" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <label htmlFor="" className="form-label">Mode</label>
+                                <select name="" id="" className="form-conrol">Online</select>
+                                <select name="" id="" className="form-conrol">Offline</select>
+                                <select name="" id="" className="form-conrol">Hybrid</select>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                                <label htmlFor="" className="form-label">Trainer</label>
+                                <input type="text" className="form-form" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
 
         </div>
-    </div>
-</div>
-
-    </div>
-  )
+    )
 }
 
 export default InsertCourse
